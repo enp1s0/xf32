15b6b77ff7840a8e8a91d9180b246fa1e7026d6b
mutsuki <mutsuki@momo86.net>
2020-07-15 19:38:14 +0900

    Update to support C++11

--- xf32.hpp
+++ xf32.hpp
@@ -17,16 +17,16 @@ union xf_bitstring {
 template <unsigned mantissa_length>
 __XF32_HOST_DEVICE__ inline float rounding(const float value) {
 	constexpr uint32_t fp32_mantissa_length = 23;
-	const uint32_t mantissa_mask = 0b0'00000000'11111111111111111111111u - (1u << (fp32_mantissa_length - mantissa_length)) - 1;
+	const uint32_t mantissa_mask = 0b00000000011111111111111111111111u - (1u << (fp32_mantissa_length - mantissa_length)) - 1;
 	const uint32_t bitstring = xf_bitstring{value}.bitstring;
 
 	const uint32_t c0 = bitstring & (1u << (fp32_mantissa_length - mantissa_length - 1));
 	const uint32_t m =  bitstring & mantissa_mask;
-	const uint32_t e =  bitstring & 0b0'11111111'00000000000000000000000u;
-	const uint32_t s =  bitstring & 0b1'00000000'00000000000000000000000u;
+	const uint32_t e =  bitstring & 0b01111111100000000000000000000000u;
+	const uint32_t s =  bitstring & 0b10000000000000000000000000000000u;
 
 	const uint32_t m0 = m + (c0 << 1);
-	const uint32_t c1 = (m0 & 0b0'00000001'00000000000000000000000u) >> 23;
+	const uint32_t c1 = (m0 & 0b00000000100000000000000000000000u) >> 23;
 	const uint32_t m_pre = m0 & mantissa_mask;
 	const uint32_t e_pre = e + (c1 << 23);
 
